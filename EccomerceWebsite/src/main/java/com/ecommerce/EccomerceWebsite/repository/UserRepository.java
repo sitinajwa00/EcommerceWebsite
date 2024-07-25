0065b60db64961cd6ab224c45185b49dae571ac0
sitinajwa00 <s.najwapandi@gmail.com>
2024-07-25 16:49:22 +0800

    Revert "Add user (test)"
    This reverts commit 045f57fe5ff8b3bc55d921bb60971f9535e5f54d.

--- EccomerceWebsite/src/main/java/com/ecommerce/EccomerceWebsite/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.EccomerceWebsite.repository;
-
-import com.ecommerce.EccomerceWebsite.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
